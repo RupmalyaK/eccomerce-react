979f8460c3c1c83d6d276aca4306efe18a02604e
RupzKoomar <rupzonthemove@gmail.com>
2020-05-17 13:42:33 +0530

    attempting to fix the heroku build bug related to babel

--- server.js
+++ server.js
@@ -12,16 +12,31 @@ import reviewRoutes from "./routes/reviewRoutes.js";
 import admin from "firebase-admin";
 import secrets from "./secrets.js"
 
-admin.initializeApp({
-    credential: admin.credential.cert(secrets),
-    databaseURL: 'https://react-rupkart-d6edf.firebaseio.com'
-  });
-
 
 if(process.env.NODE_ENV !== "production")
     {
         dotEnv.config(); 
     }
+console.log("testing env",process.env.FB_PROJECT_ID);
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+    "type": process.env.FB_TYPE ,
+    "project_id": process.env.FB_PROJECT_ID,
+    "private_key_id":process.env.FB_PRIVATE_ID,
+    "private_key":process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n') ,
+    "client_email":process.env.FB_CLIENT_EMAIL ,
+    "client_id": process.env.FB_CLIENT_ID,
+    "auth_uri": process.env.FB_AUTH_URI,
+    "token_uri": process.env.FB_TOKEN_URI,
+    "auth_provider_x509_cert_url":process.env.FB_AUTH_PROVIDER,
+    "client_x509_cert_url": process.env.FB_CLIENT_CERT,
+    }),
+    databaseURL: 'https://react-rupkart-d6edf.firebaseio.com'
+  });
+
+
+
 
 const app = express(); 
 const port = process.env.PORT || 5000; 
