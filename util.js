6f76ea8e2645cae4fbb37fb3fdf6e5128a91f749
RupzKoomar <rupzonthemove@gmail.com>
2020-05-18 19:54:01 +0530

    deleted some unused files

--- util.js
+++
@@ -1,8 +0,0 @@
-import admin from "firebase-admin"; 
-
-admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: 'https://react-rupkart-d6edf.firebaseio.com'
-  });
-
-  export default admin; 
